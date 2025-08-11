3cba41d432c8f6ecba4fc58b14d527f997dc1b0f
Pablo S <cineschonffeldt@gmail.com>
2025-08-11 11:31:53 -0500

    Add footer basic navigation

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <Link href="/privacy-policy">Privacy policy</Link>
+      <Link href="/terms-conditions">Terms and Conditions</Link>
+    </footer>
+  );
 }
