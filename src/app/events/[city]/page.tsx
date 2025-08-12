b5824774108664157e6f6cded23310e3ad8c9256
Pablo S <cineschonffeldt@gmail.com>
2025-08-12 10:57:41 -0500

    Add fetch call to get events

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -6,8 +6,13 @@ type EventsPageProps = {
   };
 };
 
-export default function EventsPage({ params }: EventsPageProps) {
+export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
+
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+  const events = await response.json();
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
@@ -15,6 +20,10 @@ export default function EventsPage({ params }: EventsPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
