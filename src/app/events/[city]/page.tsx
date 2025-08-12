d49bc40694e32f4ae9a421560231056c91612361
Pablo S <cineschonffeldt@gmail.com>
2025-08-12 11:02:49 -0500

    Add types for events

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,4 +1,5 @@
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 
 type EventsPageProps = {
   params: {
@@ -12,7 +13,7 @@ export default async function EventsPage({ params }: EventsPageProps) {
   const response = await fetch(
     "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
   );
-  const events = await response.json();
+  const events: EventoEvent[] = await response.json();
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export type EventoEvent = {
+  id: number;
+  name: string;
+  slig: string;
+  city: string;
+  location: string;
+  date: Date;
+  organizerName: string;
+  imageUrl: string;
+  description: string;
+};
