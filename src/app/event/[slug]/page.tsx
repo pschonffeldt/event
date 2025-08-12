9b00961278ea4cadf92cda4bbdfe53eefde09284
Pablo S <cineschonffeldt@gmail.com>
2025-08-12 15:14:37 -0500

    Add fetch for event url

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,15 @@
-export default function EventPage() {
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
   return <main>Event Page</main>;
 }
