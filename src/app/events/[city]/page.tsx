ff64de57c7ba3e9a66efa03cea9298f386eb2771
Pablo S <cineschonffeldt@gmail.com>
2025-08-13 13:01:54 -0500

    Move city event fetch to EventsList component and add Suspense

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,7 +1,7 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
-import { sleep } from "@/lib/util";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type EventsPageProps = {
   params: {
@@ -12,13 +12,6 @@ type EventsPageProps = {
 export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
 
-  await sleep(2000);
-
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-  const events: EventoEvent[] = await response.json();
-
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
@@ -27,7 +20,9 @@ export default async function EventsPage({ params }: EventsPageProps) {
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
