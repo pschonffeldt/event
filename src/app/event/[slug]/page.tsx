0e71fc026b29764caa83cb06c0c11099dea3a4f1
Pablo S <cineschonffeldt@gmail.com>
2025-08-13 15:09:05 -0500

    Create and implement utility function to handle city name with forst letter uppercase, also implement dynamic page title

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import H1 from "@/components/h1";
-import { sleep } from "@/lib/util";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -11,7 +10,6 @@ type EventPageProps = {
 export default async function EventPage({ params }: EventPageProps) {
   const slug = params.slug;
 
-  await sleep(2000);
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
   );
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,14 +1,11 @@
 import { EventoEvent } from "@/lib/types";
 import EventCard from "./event-card";
-import { sleep } from "@/lib/util";
 
 type EventsListProps = {
   city: string;
 };
 
 export default async function EventsList({ city }: EventsListProps) {
-  await sleep(2000);
-
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
