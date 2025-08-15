bcb45d1006071a95f6e09f9078b9058fde8b6122
Pablo S <cineschonffeldt@gmail.com>
2025-08-15 16:38:48 -0500

    Fix db connection issues

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -3,6 +3,10 @@ import { getEvent } from "@/lib/server-utils";
 import { Metadata } from "next";
 import Image from "next/image";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 type Props = {
   params: {
     slug: string;
